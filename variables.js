f13e0b33e28c9167dbdcf254d3ef2ca239923e1d
Nikita <you@example.com>
2021-05-14 11:41:28 +0530

    fetch

--- variables.js
+++ variables.js
@@ -1,2 +1,20 @@
-var sum = 10;
-console.log(sum);
+//var sum = 10;.log(sum);
+const doSomething = (action, data, method) => {
+
+    console.log('performing ${action}...');
+    method(data);
+}
+
+doSomething = ('multiply',[10,50], multiply);
+doSomething = ('division',[10,2], division);
+doSomething = ('addition',[10,20], addition);
+doSomething = ('subtraction',[30,10], subtraction);
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
